10958c224ec565440d56fc1fc2f932caa6c3058d
bekzodPythonRepository <162557417+bekzodPythonRepository@users.noreply.github.com>
2024-06-04 09:09:36 +0500

    Задания координат правого нижнего угла по осям X и Y класс QRect методы:
    setRight(<X2>)  setBottom(<Y2>)

---
+++ methodi_setRight_setBottom_class_QRect.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtCore
+
+
+# Добавьте в системные переменные PATH /путь к интерпретатору Python/Lib//site-packages//PyQt5//Qt5//plugins/
+
+r = QtCore.QRect()
+r.setRight(12)
+r.setBottom(81)
+print(r) # PyQt5.QtCore.QRect(0, 0, 13, 82)
