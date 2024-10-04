d6b7be4fe17e9e1bc84b9357c586864859694ce1
bekzodPythonRepository <162557417+bekzodPythonRepository@users.noreply.github.com>
2024-10-04 06:10:21 +0500

    Изменение цвета фона окна

---
+++ Izmenenie_cveta_fona_okna.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+# Добавьте в системные переменные PATH /путь к интерпретатору Python/Lib//site-packages//PyQt5//Qt5//plugins/
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QWidget()
+window.setWindowTitle("Изменение цвета фона окна")
+window.resize(300, 100)
+pal = window.palette()
+pal.setColor(QtGui.QPalette.Normal, QtGui.QPalette.Window, QtGui.QColor("#008800"))
+pal.setColor(QtGui.QPalette.Inactive, QtGui.QPalette.Window, QtGui.QColor("#ff0000"))
+window.setPalette(pal)
+label = QtWidgets.QLabel("Текст надписи")
+label.setAlignment(QtCore.Qt.AlignHCenter)
+label.setStyleSheet("background-color : #ffffff;")
+label.setAutoFillBackground(True)
+vbox = QtWidgets.QVBoxLayout()
+vbox.addWidget(label)
+window.setLayout(vbox)
+window.show()
+sys.exit(app.exec_())
