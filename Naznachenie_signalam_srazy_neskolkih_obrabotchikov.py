c0a73d41bd65c3ce9a1ce307c2e562b30474520b
bekzodPythonRepository <162557417+bekzodPythonRepository@users.noreply.github.com>
2024-12-22 11:40:56 +0500

    Назначение сигналом сразу нескольких обработчиков

---
+++ Naznachenie_signalam_srazy_neskolkih_obrabotchikov.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtWidgets
+import sys
+# Добавьте в системные переменные PATH /путь к интерпретатору Python/Lib//site-packages//PyQt5//Qt5//plugins/
+
+
+def on_clicked():
+    print("Кнопка нажата.Функция on_clicked()")
+
+class MyClass():
+    def __init__(self, x = 0):
+        self.x = x
+    def __call__(self):
+        print("Кнопка нажата.Метод MyClass.__call__")
+        print("x = ", self.x)
+    def on_clicked(self):
+        print("Кнопка нажата.Метод MyClass.on_clicked()")
+
+obj = MyClass()
+app = QtWidgets.QApplication(sys.argv)
+button = QtWidgets.QPushButton("Нажми меня")
+#Назначаем обработчиком функцию
+button.clicked.connect(on_clicked)
+#Назначаем обработчиком метод класса
+button.clicked.connect(obj.on_clicked)
+#Назначаем обработчиком ссылку на экземпляр класса
+button.clicked.connect(MyClass(10))
+#Назначаем обработчиком анонимную функцию
+button.clicked.connect(lambda : MyClass(5)())
+button.show()
+sys.exit(app.exec_(
