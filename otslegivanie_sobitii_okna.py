ee2497b297cc75f2fafaad2ef6d28186dc9ed8f4
bekzodPythonRepository <162557417+bekzodPythonRepository@users.noreply.github.com>
2025-04-04 00:32:33 +0500

    Отслеживание событиий окна

---
+++ otslegivanie_sobitii_okna.py
@@ -0,0 +1,32 @@
+from PyQt5 import QtCore, QtWidgets
+
+# Добавьте в системные переменные PATH /путь к интерпретатору Python/Lib//site-packages//PyQt5//Qt5//plugins/
+
+class MyWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.resize(300, 100)
+    def changeEvent(self, e):
+        if e.type() == QtCore.QEvent.WindowStateChange:
+            if self.isMinimized():
+                print("Окно свернуто")
+            elif self.isMaximized():
+                print("Окно раскрыто до максимальных размеров")
+            elif self.isFullScreen():
+                print("Полноэкранный режим")
+            elif self.isActiveWindow():
+                print("Окно находится в фокусе ввода")
+        QtWidgets.QWidget.changeEvent(self, e) #Отправляем дальше
+    def showEvent(self, e):
+        print("Окно отображено")
+        QtWidgets.QWidget.showEvent(self, e) #Отправляем дальше
+    def hideEvent(self, e):
+        print("Окно скрыто")
+        QtWidgets.QWidget.hideEvent(self, e) #Отправляем дальше
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
