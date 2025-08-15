168ce0db97f3a1b5dad22cefe9c19d30b89fa771
bekzodPythonRepository <162557417+bekzodPythonRepository@users.noreply.github.com>
2025-08-15 08:25:05 +0500

    Компонент Аккордеон Использование класса QToolBox

---
+++ ispolzovanie_classa_QToolBox.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtWidgets
+import sys
+
+# Добавьте в системные переменные PATH /путь к интерпретатору Python/Lib//site-packages//PyQt5//Qt5//plugins/
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QWidget()
+window.setWindowTitle("QToolBox")
+window.resize(200, 100)
+toolBox = QtWidgets.QToolBox()
+toolBox.addItem(QtWidgets.QLabel("Содержимое вкладки 1"), "Вкладка &1")
+toolBox.addItem(QtWidgets.QLabel("Содержимое вкладки 2"), "Вкладка &2")
+toolBox.addItem(QtWidgets.QLabel("Содержимое вкладки 3"), "Вкладка &3")
+toolBox.setCurrentIndex(0)
+vbox = QtWidgets.QVBoxLayout()
+vbox.addWidget(toolBox)
+window.setLayout(vbox)
+window.show()
+sys.exit(app.exec_())
